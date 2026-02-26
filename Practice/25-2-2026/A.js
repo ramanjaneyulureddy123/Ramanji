7b8f54becf21d9aa8eaed5efba83bccb47f704cb
Ramanjaneyulu Reddy Pandillapalli <ramanjaneyulu.pandillapalli@nybinfotech.com>
2026-02-26 09:11:29 +0530

    initial chandes

---
+++ Practice/25-2-2026/A.js
@@ -0,0 +1,47 @@
+// BOM -- browser object model -- it interact with the browser
+
+// window object 
+console.log(window.innerWidth)
+
+// screen object 
+console.log(screen.width)
+console.log(screen.height)
+
+// navigator 
+console.log(navigator.userAgent)
+
+// local object 
+console.log(location.href)
+
+// git 
+// git 4 stages
+
+// working directory 
+// -- this is our directory wher you make the project and changes the code 
+// console.log("anji")
+
+// staging area 
+// -- it is tracking
+// git add --A  
+
+// local  repositary
+// commit changes to the project before pushing 
+// git commit -m "first commit" 
+
+// central repositary
+// this is main project on the central server a copy with every team member as a local repository 
+// git push   
+
+
+// transfers your project from working directory to staging area.
+// git add .
+
+// transfers your project from staging area to Local Repository.
+// git commit -m "your message here"
+
+// transfers project from local to central repository.
+// (requires internet)
+// git push
+
+
+
